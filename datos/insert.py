b4708935aca8fd0994f28c448b53490349a3d437
Juan Jose Soto Romero <juan.sotor@upb.edu.co>
2026-03-25 15:37:38 -0500

    5

--- datos/insert.py
+++ datos/insert.py
@@ -1,13 +1,8 @@
 lista = [1, 3, 4]
-
-print("lista sin insert: ")
-print(lista)
-
+print(f"lista sin insert: {lista}")
 
 lista.insert(1, 2)
-print("lista con insert:")
-print(lista)
+print(f"lista con insert: {lista}")
 
 lista.insert(len(lista), 8)
-print("lista con insert(len(...)) ")
-print(lista) 
+print(f"lista con insert(len(...)): {lista} ")
