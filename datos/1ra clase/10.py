b11d357bc44ef600ab4d3eb38a9ab31ec0335fa7
Juan José Soto Romero <juan.sotor@upb.edu.co>
2026-03-26 05:24:31 -0500

    actu

---
+++ datos/1ra clase/10.py
@@ -0,0 +1,23 @@
+# Lista vacía
+componentes = []
+
+# Lista con elementos
+componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
+
+# Lista con diferentes tipos de datos
+datos_vuelo = [202, "Boeing 737", True, 10500.5]
+
+# Listas anidadas
+matriz_rotacion = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+
+# ¿Es posible crear listas que contengan diferentes tipos de datos?
+# Sí. Python no te obliga a que todos los elementos sean del mismo tipo.
+
+
+# ¿Una lista puede contener otra lista?
+# Sí, y a eso se le llama lista anidada, listas de listas o matríces.
+
+
+# ¿Una lista es un objeto?
+# Sí. En Python todo es un objeto, incluyendo las listas.
